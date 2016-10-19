297dc514e86147a4d073e56cfa67f183471914d5
YareArista <yarearista@gmail.com>
2016-10-19 12:11:44 -0500

    ejercicio terminado

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -8,23 +8,55 @@ function validateForm(){
 	var social = document.getElementById('input-social').value;
 	var check = document.getElementById('check').value;
 	var expresion = /\w+@\w+\.+[a-z]/;//esta es la expresion de validar el correo.
+    var span = document.createElement('span');
+    var padre = document.getElementsByClassName('input-box');
+    //var boxes = document.getElementsByClassName('input-box');
+    
+//    for(var i = 0; i < boxes.length; i++){
+//        var span = document.createElement('span');
+//        boxes[i].appendChild(span);
+//    };
+    var spans = document.getElementsByTagName('span');
+    
+    for (var i = 0; i < spans.length; i++){
+        spans[i].parentNode.removeChild(spans[i]);
+    };
 
-	if (nombre.length == 0 || apellido.length == 0 || correo.length == 0 || password == 0) {
-		//alert('Todos los campos son obligatorios');
-		return true;
-	}else if (/[a-z]/.test(nombre[0]) || /[a-z]/.test(apellido[0])) {
-		var spanNombre = document.createElement('span');
-        var contenido = document.createTextNode('Es necesario que la primer letra sea mayuscula');
-        spanNombre.appendChild(spanNombre);
-        document.nombre.appendChild(spanNombre);
+	if (nombre.length == 0) {
+		padre[0].appendChild(span);
+        span.innerHTML = 'Ingresa tu nombre';
 		return false;
-	}else if (password.length < 6 || password == 'password' || password == '123456' || password == '098754') {
-		alert('la contraseña tiene que tener como minimo seis caracteres y no deben ser consecutivos')
+	}else if (apellido.length == 0){
+        padre[1].appendChild(span);
+        span.innerHTML = 'Ingresa tu apellido';
+        return false;
+    }else if (correo.length == 0){
+        padre[2].appendChild(span);
+        span.innerHTML = 'Ingresa tu correo electronico';
+        return false;
+    }else if(password == 0){
+        padre[3].appendChild(span);
+        span.innerHTML = 'Ingresa la contraseña';
+        return false;
+    }else if (/[a-z]/.test(nombre[0])) {
+	    padre[0].appendChild(span);
+        span.innerHTML = 'La primer letra debe ser mayuscula';
+		return false;
+	}else if(/[a-z]/.test(apellido[0])){
+        padre[1].appendChild(span);
+        span.innerHTML = 'La primer letra debe ser mayuscula';
+        return false;
+    }else if (password.length < 6 || password == 'password' || password == '123456' || password == '098754') {
+		padre[3].appendChild(span);
+        span.innerHTML = 'La contraseña tiene que tener como minimo seis caracteres y no deben ser consecutivos';
+        return false;
 	}else if (!expresion.test(correo)) {
-		alert('El formato del correo electronico no es valido');
+		padre[2].appendChild(span);
+        span.innerHTML = 'La estructura del correo es invalida';
 		return false;
 	}else if( bici == null || bici == 0 ) {
-		alert('Selecciona una bicicleta');
+		padre[4].appendChild(span);
+        span.innerHTML = 'Selecciona una opcion';
   		return false;
 	};
 	
